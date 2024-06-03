a322012365179037efead9c985613a5e0a91129b
NinaCL2049 <ninaclangston@gmail.com>
2024-06-02 22:28:36 -0500

    to submit 311HW

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import axios here
+const axios = require('axios')
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
@@ -8,18 +8,50 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  return axios.get(url)
+  // console.log(axios(url))
+  // .then(function (response) {
+    
+    // return response
+  // })
 }
+
+// discoverMovie()
+
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
+  return axios.get(url)
+  .then(function(response) {
+    return response.data;
+    // console.log(response.data);
+
+  })
+  
+  // axios.get(url)
   // code here
 }
 
+getMovieById(500)
+
+
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
+  return axios.get(url)
+  .then(function(response) {
+    return response.data;
+  
+
+  })
+  .catch(error => {
+    return error.response.status;
+    // console.log(error.response.status);
+  })
+  
   // code here
 }
+// console.log(error.response.status)
 
 
 
@@ -28,3 +60,9 @@ module.exports = {
   getMovieById,
   getMovieByIdFailure
 }
+
+// from tmdb documentation:
+
+// curl --request GET \
+//      --url 'https://api.themoviedb.org/3/movie/343611?api_key=7a002ed6c2c1bee5ffc5c6a3d0d96f77'
+// npm test index.test.js
--- test/index.test.js
+++ test/index.test.js
@@ -17,12 +17,12 @@ describe('API protocol practice', () => {
     expect(result).toHaveProperty('headers')
   })
 
-  test('It should get a movie by it\'s id', async () => {
+  test('It should get a movie by its id', async () => {
     const data = await getMovieById(500)
     expect(data.original_title).toBe('Reservoir Dogs')
   })
 
-  test('It should FAIL to get a movie by it\'s id and return a 404', async () => {
+  test('It should FAIL to get a movie by its id and return a 404', async () => {
     const status = await getMovieByIdFailure()
     expect(status).toEqual(404)
   })
